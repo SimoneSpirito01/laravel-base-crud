f059a2f1ccb6f82662a3d8d8027cb6d09c75b4b4
SimoneSpirito <simospiri01@gmail.com>
2022-02-09 16:57:40 +0100

    add migration and model

--- database/migrations/2022_02_09_153459_create_comics_table.php
+++ database/migrations/2022_02_09_153459_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb', 100);
+            $table->float('price', 6, 4);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
